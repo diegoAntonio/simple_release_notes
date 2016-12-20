568d5b2d6fbe9212019d9eb2cca0212386fc1ea5
diego.ferreira <diego.ferreira@consensotec.com.br>
2016-12-20 17:47:14 -0300

    remocao do path de testes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-get '/test' => 'release#index'
+get '/release_notes' => 'release#index'
 
 get '/gerar_release' => 'release#generate_release', as: 'gerar_release'
