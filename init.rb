4aa6285f3f318c59cdce9d4666ce44a6c8ec7487
diego.ferreira <diego.ferreira@consensotec.com.br>
2016-12-20 17:37:47 -0300

    adicao de locale e inicio do controle de acesso

--- init.rb
+++ init.rb
@@ -1,9 +1,10 @@
 Redmine::Plugin.register :simple_release_notes do
   name 'Simple Release Notes plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Diego Antônio Ferreira'
+  description 'A simple plugin for generate automatic Release Notes'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
-  menu :application_menu, :simple_release_notes, { :controller => 'release', :action => 'index' }, :caption => 'Release Notes'
+  url 'http://github.com/diegoAntonio/SimpleReleaseNotes'
+  author_url 'http://github.com/diegoAntonio'
+  menu :top_menu , :simple_release_notes, { :controller => 'release', :action => 'index' }, :caption => 'Release Notes'
+  permission :generate_release, :release => :index
 end
