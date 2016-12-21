6497162e8f9940c1f5aa4f7583785094ab3ae62f
diego.ferreira <diego.ferreira@consensotec.com.br>
2016-12-21 09:03:33 -0300

    inclusao de pasta de assets para importar arquivos de forma correta

--- init.rb
+++ init.rb
@@ -5,6 +5,7 @@ Redmine::Plugin.register :simple_release_notes do
   version '0.0.1'
   url 'http://github.com/diegoAntonio/SimpleReleaseNotes'
   author_url 'http://github.com/diegoAntonio'
-  menu :top_menu , :simple_release_notes, { :controller => 'release', :action => 'index' }, :caption => 'Release Notes'
+  menu :application_menu  , :simple_release_notes, { :controller => 'release', :action => 'index' }, :caption => 'Release Notes'
   permission :generate_release, :release => :index
+  permission :gerar_release, :release => :generate_release
 end
