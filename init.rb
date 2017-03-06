68ea70849591321b732a39ba4828b07695c26f2d
diego.ferreira <diego.ferreira@consensotec.com.br>
2017-03-06 17:45:07 -0300

    ajuste de versionamento

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :simple_release_notes do
   name 'Simple Release Notes plugin'
   author 'Diego Antônio Ferreira'
   description 'A simple plugin for generate automatic Release Notes'
-  version '0.1.0'
+  version '1.1.0'
   url 'http://github.com/diegoAntonio/SimpleReleaseNotes'
   author_url 'http://github.com/diegoAntonio'
   menu :project_menu  , :simple_release_notes, { :controller => 'release', :action => 'index' }, :caption => 'Release Notes'
