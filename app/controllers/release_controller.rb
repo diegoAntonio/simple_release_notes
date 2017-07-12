e25d2bdeaba73a100fdb408bb58c788be8c493d3
diego.ferreira <diego.ferreira@consensotec.com.br>
2017-07-12 09:51:16 -0300

    RM #26005 ID #26005 UC0000 - Inclusao de funcionalidade para inserir RM's avulsas no release notes.

--- app/controllers/release_controller.rb
+++ app/controllers/release_controller.rb
@@ -17,6 +17,9 @@ class ReleaseController < ApplicationController
     template = path + '/' + 'template_consenso.odt'
     time = Time.now
     is_private_false = 0
+    custom_issues_lst = nil
+    custom_msg = ' '
+    parameter_custom_issues = params[:custom_issues]
 
     id_pronta_pra_homolog = get_propertie("id_pronta_pra_homolog")
     id_concluida = get_propertie("id_concluida")
@@ -30,12 +33,24 @@ class ReleaseController < ApplicationController
                           :is_private => is_private_false)
                           .order("tracker_id","id")
 
+    if !parameter_custom_issues.nil? && !parameter_custom_issues.empty?
+      custom_issues_lst = Issue.where(:id => parameter_custom_issues)
+      custom_msg = '* significa RM\'s entregues de forma parcial.'
+      
+      custom_issues_lst.each do |issue|
+        issue.subject = issue.subject + "*"
+      end
+
+      @issues.push(*custom_issues_lst)
+    end  
+
     report = ODFReport::Report.new(template) do |r|
       r.add_field  :VERSION_NAME, @selected_version.name
       r.add_field  :RELEASE_DATE, time.strftime("%d/%m/%Y")
       r.add_field  :PRODUCT_NAME, params[:product]
-      r.add_field  :TARGET_ENV, params[:environment]
-
+      r.add_field  :TARGET_ENV,  params[:environment]
+      r.add_field  :CUSTOM_RM_MESSAGE, custom_msg
+    
       r.add_table("rms_table", @issues, :header=>true) do |t|
         t.add_column(:RM_ID, :id || "")
         t.add_column(:RM_SUBJECT, :subject || "") 
