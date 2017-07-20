8b43b362be104a7ef3e65977adcf55cf3c59b6eb
diego.ferreira <diego.ferreira@consensotec.com.br>
2017-07-20 09:32:33 -0300

    RM #26005 ID #26005 UC0000 - Commit de Versionamento para nova versão do simple release notes com a nova funcionalidade de Inclusao de RM's avulsas - Correcao de bug

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :simple_release_notes do
   name 'Simple Release Notes plugin'
   author 'Diego Antônio Ferreira'
   description 'A simple plugin for generate automatic Release Notes'
-  version '1.3.1'
+  version '1.3.2'
   url 'http://github.com/diegoAntonio/SimpleReleaseNotes'
   author_url 'http://github.com/diegoAntonio'
   menu :project_menu  , :simple_release_notes, { :controller => 'release', :action => 'index' }, :caption => 'Release Notes'
